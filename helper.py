e048c968a76991e4f07ec5a935f7ac3a91413834
David <davidhil57@gmail.com>
2018-08-21 15:16:58 +0200

    Added log()-function

--- helper.py
+++ helper.py
@@ -1,10 +1,13 @@
 import math
 
-def parseTime(timepassed):
-    hours = math.trunc(timepassed/3600)
-    timepassed -= 3600*hours
-    minutes = math.trunc(timepassed/60)
-    timepassed -= 60 * minutes
-    seconds = math.trunc(timepassed)
+def parseTime(secspassed):
+    hours = math.trunc(secspassed/3600)
+    secspassed -= 3600*hours
+    minutes = math.trunc(secspassed/60)
+    secspassed -= 60 * minutes
+    seconds = math.trunc(secspassed)
     output = " {}h {}:{}min".format(hours, minutes if minutes > 9 else "0{}".format(minutes), seconds if seconds > 9 else "0{}".format(seconds))
     return output
+
+def parseTask(task):
+    return "{} {}".format(task[2] if task[2] else "task", "[{}]".format(task[1]) if task[1] else "")
--- stechuhr.py
+++ stechuhr.py
@@ -6,9 +6,10 @@ import time
 import helper
 
 parser = argparse.ArgumentParser(description="A simple time stamp clock for the command line")
-parser.add_argument("command", help="the command that will be executed", choices=['start','stop', 'status'])
+parser.add_argument("command", help="the command that will be executed", choices=['start','stop', 'status', 'log'])
 parser.add_argument("-l", "--label", help="the label of the task you're working on (use this for categories like \"Work\")")
 parser.add_argument('-t', '--task', help="the task you're working on (use this as a specific description like \"Finishing feature x\")")
+parser.add_argument("-n", '--number', help="how many of the last tasks horologium log shows (default is 3)", type=int, default=3)
 
 if os.path.isfile('stechuhr.db'):
     db = sqlite3.connect("stechuhr.db")
@@ -16,8 +17,8 @@ if os.path.isfile('stechuhr.db'):
 else:
     db = sqlite3.connect("stechuhr.db")
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE status(date text, label text, task text)")
-    cursor.execute("CREATE TABLE history(date text, label text, task text, time real)")
+    cursor.execute("CREATE TABLE status(date real, label text, task text)")
+    cursor.execute("CREATE TABLE history(date real, label text, task text, time real)")
 db.commit()
 
 args = parser.parse_args()
@@ -29,7 +30,7 @@ def start():
     if cursor.fetchone():
         print("You're already working on a task. For more info, use horologium status")
         return
-    cursor.execute("INSERT INTO status VALUES (?,?,?)", (time.asctime(time.localtime(time.time())), args.label, args.task))
+    cursor.execute("INSERT INTO status VALUES (?,?,?)", (time.time(), args.label, args.task))
     print("Started time measurement for {}{}.".format(args.task if args.task else "task", " [{}]".format(args.label) if args.label else ""))
     cursor.execute
     db.commit()
@@ -40,8 +41,8 @@ def status():
     if not task:
         print("You aren't currently working on a task. Use horologium start to start working.")
     else:
-        timepassed = time.time() - time.mktime(time.strptime(task[0]))
-        print("You're working on {} {} for".format(task[2] if task[2] else "task", "[{}]".format(task[1]) if task[1] else "") + helper.parseTime(timepassed))
+        timepassed = time.time() - task[0]
+        print("You're working on " + helper.parseTask(task) + "for" + helper.parseTime(timepassed))
 
 def stop():
     cursor.execute("SELECT * FROM status")
@@ -49,16 +50,22 @@ def stop():
     if not task:
         print("You aren't currently working on a task. Use horologium start to start working.")
     else:
-        timepassed = time.time() - time.mktime(time.strptime(task[0]))
+        timepassed = time.time() - task[0]
         cursor.execute("INSERT INTO history VALUES (?,?,?,?)", (task[0],task[1],task[2],timepassed))
         db.commit()
         cursor.execute("DELETE FROM status WHERE 1=1")
         db.commit()
-        print("You were working on {} {} for".format(task[2] if task[2] else "task", "[{}]".format(task[1]) if task[1] else "") + helper.parseTime(timepassed))
-    
+        print("You were working on " + helper.parseTask(task) + "for" + helper.parseTime(timepassed))
+
+def log():
+    cursor.execute("SELECT * FROM history ORDER BY date DESC LIMIT ?", str(args.number))
+    rows = cursor.fetchall()
+    for task in rows:
+        print("On " + time.asctime(time.localtime(task[0])) + " you worked for " + helper.parseTime(task[3]) + " on " + helper.parseTask(task))
 
 
-functions = {"start": start, "status": status, "stop": stop}
+
+functions = {"start": start, "status": status, "stop": stop, "log" : log}
 functions[args.command]()
 
 db.close()
